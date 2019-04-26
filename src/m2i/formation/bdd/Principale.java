725a981e42c52c0eccc4282382c5de6202225388
Emmanuel De Oliveira <38254662+emdeo@users.noreply.github.com>
2019-04-26 16:35:05 +0200

    Add files via upload

--- src/m2i/formation/bdd/Principale.java
+++ src/m2i/formation/bdd/Principale.java
@@ -2,6 +2,7 @@ package m2i.formation.bdd;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -9,22 +10,31 @@ import java.sql.Statement;
 public class Principale {
 
 	public static void main(String[] args) {
-		// url = "protocole//nom_de_hote:port_de_communication/nom_de_ma_BD"
-		// (Nécessite de télécharger mysql-connector-java-8.0.16 et de l'importer :
-		// "Project Properties" -> "Java Build Path" -> "Libraries" -> "ClassPath" ->
-		// "Add External JARs")
-		String url = "jdbc:mysql://localhost:3306/m2i-formation?serverTimezone=UTC";
+		/*
+		 * url = "protocole//nom_de_hote:port_de_communication/nom_de_ma_BD"
+		 * 
+		 * (Nécessite de télécharger mysql-connector-java-8.0.16 et de l'importer :
+		 * "Project Properties" -> "Java Build Path" -> "Libraries" -> "ClassPath" ->
+		 * "Add External JARs")
+		 * 
+		 * Vérifier également que MySQL tourne bien sur XAMPP
+		 */
+		String url = "jdbc:mysql://localhost:3306/sakila?serverTimezone=UTC";
 
 		String user = "root";
 		String password = "";
 
-		String chSql = "select * from personne"; // mes instructions (requêtes) SQL
+		String chSql = "select * from film"; // mes instructions (requêtes) SQL
+//		String chSql_insert = "insert into Personne (ID, Nom, Prénom, Téléphone, Email) values (?, ?, ?, ?, ?)";
 
 		Statement st = null;
+		PreparedStatement ps = null;
 		ResultSet rs = null; // stockera le résultat de ma requête SQL
 
-		// exécuter cette instruction si tout va bien (nécessaire car sinon lance une
-		// exception)
+		/*
+		 * Exécuter cette instruction si tout va bien (nécessaire car sinon lance une
+		 * exception)
+		 */
 		try {
 
 			Connection cnn = DriverManager.getConnection(url, user, password); // établir connection à ma base de
@@ -33,15 +43,28 @@ public class Principale {
 			// On crée une requête SQL
 			st = cnn.createStatement();
 
-			// Récupérer un jeu d'enregistrements (= result set). Prend une requête SQL en
-			// paramètre
+			/*
+			 * Récupérer un jeu d'enregistrements (= result set). Prend une requête SQL en
+			 * paramètre
+			 */
 			rs = st.executeQuery(chSql);
 
 			while (rs.next()) {
 				AfficherDB(rs); // sysout() de tous les élément de la BD passée en prm
 			}
 
-		} // sinon, exécuter cette instruction
+//			ps = cnn.prepareStatement(chSql_insert);
+//			ps.setInt(1, 103); // insérer l'index d'un paramètre et sa valeur (ici = ID) -> LE DECOMPTE
+//								// COMMENCE A 1 !!!!
+//			ps.setString(2, "Dupont");
+//			ps.setString(3, "Didier");
+//			ps.setString(4, "0678915423");
+//			ps.setString(5, "didudipont@lol.org");
+//			
+//			System.out.println(ps.executeUpdate());
+//			System.out.println(ps.toString());
+
+		} // Sinon, exécuter cette instruction
 		catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -51,15 +74,15 @@ public class Principale {
 	public static void AfficherDB(ResultSet rs) {
 		try {
 			// prend un nom de colonne en prm
-			int id = rs.getInt("ID");
-			String nom = rs.getString("Nom");
-			String prenom = rs.getString("Prénom");
-			String tel = rs.getString("Téléphone");
-			String email = rs.getString("Email");
-			int nbVictoires = rs.getInt("NbVictoires");
+			int id = rs.getInt("film_id");
+			String title = rs.getString("title");
+			String descr = rs.getString("description");
+			int release_year = rs.getInt("release_year");
+			int rental_duration = rs.getInt("rental_duration");
+			float rental_rate = rs.getFloat("rental_rate");
 
 			// On vérifie que la BD a bien été chargée
-			System.out.println(id + "\t" + nom + "\t" + prenom + "\t" + tel + "\t" + email);
+			System.out.println(id + "\t" + title + "\t" + release_year + "\t" + rental_duration + "\t" + rental_rate);
 
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
